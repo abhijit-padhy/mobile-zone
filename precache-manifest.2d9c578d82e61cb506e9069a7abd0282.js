55f87e7b9b440350d9cb11daea615df86598bbf6
abhijitp <abhijitp@orowealth.com>
2020-04-29 19:39:19 +0530

    Updates

---
+++ precache-manifest.2d9c578d82e61cb506e9069a7abd0282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186533badaec75db4c0d1888667862de",
+    "url": "/mobile-zone/index.html"
+  },
+  {
+    "revision": "f248a90a040c4b581037",
+    "url": "/mobile-zone/static/css/main.b39c7e74.chunk.css"
+  },
+  {
+    "revision": "4835ba2a112c14e900fa",
+    "url": "/mobile-zone/static/js/2.33704d42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-zone/static/js/2.33704d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f248a90a040c4b581037",
+    "url": "/mobile-zone/static/js/main.92844f56.chunk.js"
+  },
+  {
+    "revision": "5f0d9c83a1615a25b80f",
+    "url": "/mobile-zone/static/js/runtime-main.d5aeec30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobile-zone/static/media/logo.5d5d9eef.svg"
+  }
+]);
